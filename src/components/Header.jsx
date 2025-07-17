53a8ed64bcc8f3058fce9578e7ebc1f61d25bfe9
Divyanshu <lyricalv15@gmail.com>
2025-07-17 10:42:02 +0530

    Started project

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import {logo} from "../../public/assets/Khetify-logo.png"
+
+const Header = () => {
+    return(
+        <div className="navbar"></div>
+    );
+};
+
+
+
+
+
+
+
+
+
+
+
+
+export default Header;
